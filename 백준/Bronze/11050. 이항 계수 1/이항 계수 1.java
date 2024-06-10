eeb8be1d4c3ffe2a03f21a0b6efed13024760624
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2024-06-11 00:08:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,19 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int combination(int n, int r) {
-        if (n == r || r == 0) return 1;
-
-        return combination(n - 1, r - 1) + combination(n - 1, r);
-    }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        System.out.println(combination(N, K));
+        int[][] dp = new int[N + 1][K + 1];
+
+        System.out.println(combination(N, K, dp));
+    }
+
+    static int combination(int n, int k, int[][] dp) {
+        if (dp[n][k] > 0) return dp[n][k];
+
+        if (n == k || k == 0) return dp[n][k] = 1;
+
+        return combination(n-1, k-1, dp) + combination(n-1, k, dp);
     }
 }
