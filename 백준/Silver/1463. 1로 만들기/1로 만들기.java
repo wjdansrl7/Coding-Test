72341646b4a3ffce02871388d63c47ec29215270
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2024-06-11 14:53:27 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,41 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int res = Integer.MAX_VALUE;
-
-    static class Node implements Comparable<Node>{
-        int v;
-        int cnt;
-
-        public Node(int v, int cnt) {
-            this.v = v;
-            this.cnt = cnt;
-        }
-        @Override
-        public int compareTo(Node o) {
-            if (this.cnt == o.cnt) return this.v - o.v;
-            return this.cnt - o.cnt;
-        }
-    }
+    static int[] dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int x = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dp = new int[x + 1];
 
-        pq.offer(new Node(x, 0));
+        dp[0] = 0;
+        dp[1] = 0;
 
-        while (!pq.isEmpty()) {
-            Node p = pq.poll();
-            if (p.v == 1) {
-                res = res > p.cnt ? p.cnt : res;
-                break;
-            }
-            if (p.v % 3 == 0) pq.offer(new Node(p.v/3, p.cnt+1));
-            if (p.v % 2 == 0) pq.offer(new Node(p.v/2, p.cnt+1));
-            pq.offer(new Node(p.v-1, p.cnt+1));
+        for (int i = 2; i < x + 1; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2]+1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3]+1);
         }
-        System.out.println(res);
+        System.out.println(dp[x]);
+
     }
 }
