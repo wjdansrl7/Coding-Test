72ad18d4f7b9eb2a0e5bd49c5855aa043cff95a3
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2024-06-18 13:48:48 +0900

    [Silver I] Title: 트럭, Time: 96 ms, Memory: 11888 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,46 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int n, w, l;
+    static int[] truck;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n, w, l;
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
         l = Integer.parseInt(st.nextToken());
 
-        ArrayDeque<Integer> truck = new ArrayDeque<>();
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            truck.offer(Integer.parseInt(st.nextToken()));
-        }
+        truck = new int[n];
 
         ArrayDeque<Integer> q = new ArrayDeque<>();
 
-        for (int i = 0; i < w; i++) {
-            q.offer(0);
-        }
+        for (int i = 0; i < w; i++) q.offer(0);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) truck[i] = Integer.parseInt(st.nextToken());
 
-        int time = 0, sum = 0;
+        int time = 0, sum = 0, idx = 0;
         while (!q.isEmpty()) {
-            time++;
             sum -= q.poll();
 
-            if (!truck.isEmpty()) {
-                if (truck.peek() + sum <= l) {
-                    sum += truck.peek();
-                    q.offer(truck.poll());
+            if (idx < n) {
+                if (sum + truck[idx] <= l) {
+                    q.offer(truck[idx]);
+                    sum += truck[idx++];
                 } else {
                     q.offer(0);
                 }
             }
+            time++;
         }
-
         System.out.println(time);
-
     }
 }
