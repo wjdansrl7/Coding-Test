e4fa8c7ac7c87b31b26303e1e713aeac4b5e5645
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2024-06-22 22:53:22 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 456 ms, Memory: 125336 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,8 +1,10 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /*
 LIS에 대한 구현 방식 -> 이진 탐색을 활용하여 O(nlogN)을 이용하여 해결한다.
+배열을 이용하여 시간 비교해보기.
  */
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -11,26 +13,30 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        List<Integer> LIS = new ArrayList<>();
-        LIS.add(0);
+        int[] LIS = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int value = arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-            if (LIS.get(LIS.size() - 1) < value) LIS.add(value);
+        LIS[0] = arr[0];
+        int len = 1;
+
+        for (int i = 1; i < N; i++) {
+            int key = arr[i];
+
+            if (LIS[len - 1] < key) LIS[len++] = key;
             else {
-                int start = 0, end = LIS.size()-1;
+                int start = 0, end = len;
 
                 while (start < end) {
-                    int mid = (start + end) >> 1;
+                    int mid = (start+end) >> 1;
 
-                    if (LIS.get(mid) >= value) end = mid;
+                    if (LIS[mid] >= key) end = mid;
                     else start = mid + 1;
                 }
-                LIS.set(end, value);
+                LIS[end] = key;
             }
         }
-        System.out.println(LIS.size()-1);
+        System.out.println(len);
     }
 }
