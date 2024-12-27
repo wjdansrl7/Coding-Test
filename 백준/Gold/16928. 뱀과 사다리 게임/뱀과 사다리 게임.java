c668f2da46090e3050f954acd10f9a05e000f7f8
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2024-12-27 20:05:30 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 11740 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,76 +1,62 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 /**
  * packageName    : _241227
- * fileName       : BOJ_G5_16928_뱀과사다리게임
+ * fileName       : BOJ_G5_16928_뱀과사다리게임_최적화
  * author         : moongi
  * date           : 12/27/24
  * description    :
  */
 public class Main {
-    static int N, M, res = 18;
-    static int[] visited;
-    static class Dice {
-        int target;
-        int cnt;
-
-        public Dice(int target, int cnt) {
-            this.target = target;
-            this.cnt = cnt;
-        }
-    }
-    // 잊지말자.
-    static class DiceComparator implements Comparator<Dice> {
-        @Override
-        public int compare(Dice o1, Dice o2) {
-            return Integer.compare(o2.target, o1.target);
-        }
-    }
+    static int N, M;
+    static Map<Integer, Integer> map;
+    static boolean[] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        visited = new int[101];
-        Arrays.fill(visited, 18);
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 1; i < 101; i++) map.put(i, map.getOrDefault(i, i));
+        map = new HashMap<>();
+        visited = new boolean[101];
 
         for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
             map.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        PriorityQueue<Dice> maxHeap = new PriorityQueue<>(new DiceComparator());
+        System.out.println(bfs());
 
-//        PriorityQueue<Dice> m = new PriorityQueue<>(new Comparator<Dice>() {
-//            @Override
-//            public int compare(Dice o1, Dice o2) {
-//                return Integer.compare(o2.target, o1.target);
-//            }
-//        });
+    }
 
-        maxHeap.offer(new Dice(1, 0));
+    static int bfs() {
+        int res = 0;
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(1);
+        visited[1] = true;
 
-        while (!maxHeap.isEmpty()) {
-            Dice curr = maxHeap.poll();
-            if (curr.cnt >= res) continue;
+        while (!q.isEmpty()) {
+            res++;
+            // queue 크기가 변동이 있기 떄문에 고정을 시켜줘야 한다.
+            for (int i = 0, qSize = q.size(); i < qSize; i++) {
+                int curr = q.poll();
 
-            if (curr.target == 100) {
-                res = curr.cnt;
-                continue;
-            }
-            for (int i = 1; i <= 6; i++) {
-                if (curr.target+i <= 100 && visited[map.get(curr.target + i)] >= curr.cnt + 1) {
-                    visited[map.get(curr.target + i)] = curr.cnt + 1;
-                    maxHeap.offer(new Dice(map.get(curr.target+i), visited[map.get(curr.target + i)]));
+                for (int j = 1; j < 7; j++) {
+                    int move = curr + j;
+
+                    if (move == 100) return res;
+                    if (move > 100) continue;
+                    if (visited[move]) continue;
+
+                    visited[move] = true;
+                    if (map.containsKey(move)) {
+                        move = map.get(move);
+                    }
+                    q.offer(move);
                 }
             }
         }
-        System.out.println(res);
+        return res;
     }
 }
