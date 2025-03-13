1ec2eb4d401f19892de762df47d8c90561190470
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-03-13 19:51:37 +0900

    [Silver II] Title: 색종이 만들기, Time: 100 ms, Memory: 13164 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,48 +1,58 @@
+
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
 	static int[][] arr;
-	static int b = 0, w = 0;
-	static void makePaper(int N, int x, int y) {
-		boolean flag = true;
-		label: for (int i = x; i < x+N; i++) {
-			for (int j = y; j < y+N; j++) {
-				if(arr[x][y] != arr[i][j]) {
-					flag = false;
-					break label;
-				}
-			}
-		}
-		if(flag) {
-			if(arr[x][y] == 0) w++;
-			else b++;
-		} else {
-			makePaper(N >> 1, x, y);
-			makePaper(N >> 1, x, y + (N >> 1));
-			makePaper(N >> 1, x + (N >> 1), y);
-			makePaper(N >> 1, x + (N >> 1), y + (N >> 1));
-		}
-		
-		
-	}
-	public static void main(String[] args) throws Exception{
+	static int[] count;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		
+		// white: 0, blue: 1
+
+		int N = Integer.parseInt(br.readLine());
 		arr = new int[N][N];
+		count = new int[2];
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		makePaper(N, 0, 0);
-		
-		System.out.println(w);
-		System.out.println(b);
+
+		makePaper(0, 0, N);
+
+		System.out.println(count[0]);
+		System.out.println(count[1]);
+	}
+
+	static void makePaper(int x, int y, int size) {
+		if (cuttable(x, y, size)) {
+			if (arr[x][y] == 0)
+				count[0]++;
+			else
+				count[1]++;
+			return;
+		}
+
+		int newSize = size / 2;
+
+		makePaper(x, y, newSize);
+		makePaper(x, y + newSize, newSize);
+		makePaper(x + newSize, y, newSize);
+		makePaper(x + newSize, y + newSize, newSize);
+	}
+
+	static boolean cuttable(int x, int y, int size) {
+		int cur = arr[x][y];
+
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				if (cur != arr[i][j])
+					return false;
+			}
+		}
+		return true;
 	}
 }
