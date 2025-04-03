61eedf22091131924b01b69e0a818d4026461911
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-04-03 21:43:07 +0900

    [Gold V] Title: 틱택토, Time: 72 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -1,136 +1,76 @@
 import java.util.*;
 import java.io.*;
 
-/**
- * 빈 칸이 있을 경우, col, row, cross 를 통해 확인
- * xCount - yCount == 1 or xCount == yCount
- * 예외처리하려면 X, O 둘 다 구해야함.
- *
- * 말이 다른 경우, X가 승리해야한다
- * 말의 개수가 같은 경우, O가 승리해야 한다.
- * invalid한 경우
- * 모두 채우지 않았는데 개수가 xCount <= 2 && yCount <= 2
- * 모두 채우지 않았고, 게임이 성공하지 않은 경우
- * 모두 채우지 않았고, 게임은 성공했지만, 자신의 말이 아닌 다른 말도 게임에서 성공한 경우
- * 모두 채웠는데, 자신의 말이 아닌 다른 말이 게임에서 승리한 경우
- */
 public class Main {
-    static boolean rowValid(char m) {
-        for (int i = 0; i < 3; i++) {
-            int cnt = 0;
-            if(arr[i][0] != m) continue;
+	static char[][] board;
+	static int xCnt, oCnt;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-            for (int j = 0; j < 3; j++) {
-                if(arr[i][j] == m) cnt++;
-            }
+		board = new char[3][3];
 
-            if(cnt == 3) return true;
-        }
-        return false;
-    }
+		while (true) {
 
-    static boolean crossValid(char m) {
-        int cnt = 0;
-        for (int i = 0; i < 3; i++) {
-            if (arr[i][i] == m) cnt++;
-        }
+			String str = br.readLine();
 
-        if (cnt == 3) return true;
+			if (str.equals("end")) break;
 
-        cnt = 0;
+			xCnt = 0;
+			oCnt = 0;
 
-        for (int i = 0; i < 3; i++) {
-            if(arr[i][2-i] == m) cnt++;
-        }
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					board[i][j] = str.charAt(i * 3 + j);
 
-        if(cnt == 3) return true;
+					if (board[i][j] == 'X') xCnt++;
+					else if (board[i][j] == 'O') oCnt++;
+				}
+			}
 
-        return false;
-    }
+			// 보드판을 다 채운 경우
+			if (xCnt + oCnt == 9 && xCnt == oCnt + 1) {
 
-    static boolean colValid(char m) {
-        for (int i = 0; i < 3; i++) {
-            int cnt = 0;
-            if(arr[0][i] != m) continue;
+				// 둘이 동시에 이기는 경우는 불가능
+				if (check('X') && check('O')) sb.append("invalid").append('\n');
+				// 두 번째 사람이 이기는 것은 불가능
+				else if (check('O')) sb.append("invalid").append('\n');
+				// 이외의 경우
+				else sb.append("valid").append('\n');
+			} else {
+				// 다 채우지 못한 경우
+				if (check('X') && check('O')) sb.append("invalid").append('\n');
+				else if (check('X') && xCnt == oCnt + 1) sb.append("valid").append('\n');
+				else if (check('O') && xCnt == oCnt) sb.append("valid").append('\n');
+				else sb.append("invalid").append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
 
-            for (int j = 0; j < 3; j++) {
-                if(arr[j][i] == m) cnt++;
-            }
+	static boolean check(char c) {
 
-            if(cnt == 3) return true;
-        }
-        return false;
-    }
+		// 가로
+		for (int i = 0; i < 3; i++) {
+			int cnt = 0;
+			for (int j = 0; j < 3; j++) {
+				if (board[i][j] == c) cnt++;
+ 			}
+			if (cnt == 3) return true;
+		}
+		// 세로
+		for (int i = 0; i < 3; i++) {
+			int cnt = 0;
+			for (int j = 0; j < 3; j++) {
+				if (board[j][i] == c) cnt++;
+			}
+			if (cnt == 3) return true;
+		}
 
-    static int xCount, yCount;
-    static boolean flag;
-    static char[][] arr;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		// 대각선
+		if (board[0][0] == c && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) return true;
+		if (board[0][2] == c && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0])) return true;
 
-        arr = new char[3][3];
-        while (true) {
-
-            String str = br.readLine();
-
-            flag = false;
-            xCount = 0;
-            yCount = 0;
-
-            if (str.equals("end")) return;
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    arr[i][j] = str.charAt(i * 3 + j);
-
-                    if (arr[i][j] == 'X') xCount++;
-                    else if(arr[i][j] == 'O') yCount++;
-                }
-            }
-            // 1. 차이가 1을 넘어가거나, O의 개수가 더 많은 경우
-            if(xCount - yCount > 1 || xCount < yCount) {
-                flag = true;
-            }
-
-            if(flag) {
-                System.out.println("invalid");
-                continue;
-            }
-
-            if(xCount == 5 && yCount == 4) {
-                // 모두 채운 경우
-                if (rowValid('O') || colValid('O') || crossValid('O')) {
-                    // true가 나올 경우, invalid한 경우
-                    System.out.println("invalid");
-                } else {
-                    // valid
-                    System.out.println("valid");
-                }
-            } else {
-                // 모두 채우지 못한 경우
-                boolean xx = false, yy = false;
-                if(rowValid('X') || colValid('X') || crossValid('X')) {
-                    xx = true;
-                }
-                if(rowValid('O') || colValid('O') || crossValid('O')) {
-                    yy = true;
-                }
-
-                // xCount - yCount = 1
-                if(xCount - yCount == 1) {
-                    if(xx && !yy) System.out.println("valid");
-                    else System.out.println("invalid");
-                }
-                if(xCount == yCount) {
-                    if(!xx && yy) System.out.println("valid");
-                    else System.out.println("invalid");
-                }
-
-            }
-
-
-
-        }
-    }
+		return false;
+	}
 }
