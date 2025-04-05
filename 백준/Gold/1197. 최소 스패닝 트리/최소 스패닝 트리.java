d4ab4dbada52d4066c3e02b0e2da3dede76a0859
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-04-05 18:12:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 480 ms, Memory: 58680 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,65 +1,69 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int V, E;
+	static int V, E;
+	static class Node implements Comparable<Node> {
+		int to;
+		int weight;
 
-    static class Node implements Comparable<Node>{
-        int to, weight;
+		public Node(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
 
-        public Node(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
-    }
-    static List<Node>[] nodes;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		@Override
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
+		}
+	}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+	static List<Node>[] nodes;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        nodes = new List[V + 1];
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < V + 1; i++) nodes[i] = new ArrayList<>();
+		nodes = new List[V + 1];
 
-        boolean[] v = new boolean[V+1];
+		for (int i = 0; i < V + 1; i++) {
+			nodes[i] = new ArrayList<Node>();
+		}
+		int a, b, w;
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
 
-        int from, to, weight;
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            from = Integer.parseInt(st.nextToken());
-            to = Integer.parseInt(st.nextToken());
-            weight = Integer.parseInt(st.nextToken());
+			nodes[a].add(new Node(b, w));
+			nodes[b].add(new Node(a, w));
+		}
 
-            nodes[from].add(new Node(to, weight));
-            nodes[to].add(new Node(from, weight));
-        }
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[V + 1];
+		long res = 0;
 
-        int result = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(1, 0));
 
-        pq.offer(new Node(1, 0));
+		while (!pq.isEmpty()) {
+			Node curr = pq.poll();
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
+			if (visited[curr.to]) continue;
+			visited[curr.to] = true;
 
-            if (v[cur.to]) continue;
+			res += curr.weight;
 
-            v[cur.to] = true;
-            result += cur.weight;
+			for (Node node : nodes[curr.to]) {
+				if (!visited[node.to]) {
+					pq.offer(new Node(node.to, node.weight));
+				}
+			}
+		}
 
-            for (Node node : nodes[cur.to]) {
-                if (!v[node.to]) {
-                    pq.add(node);
-                }
-            }
-        }
-        System.out.println(result);
-    }
+		System.out.println(res);
+	}
 }
