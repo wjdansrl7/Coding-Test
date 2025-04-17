38d51d375cc1e91655e59bb44b2de70b5a164622
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-04-17 14:47:31 +0900

    [Silver V] Title: 올림픽, Time: 84 ms, Memory: 12104 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -3,34 +3,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Nation implements Comparable<Nation> {
-		int num;
-		int gold;
-		int silver;
-		int bronze;
 
-		public Nation(int num, int gold, int silver, int bronze) {
-			this.num = num;
-			this.gold = gold;
-			this.silver = silver;
-			this.bronze = bronze;
-		}
-
-		@Override
-		public int compareTo(Nation o) {
-			if (this.gold == o.gold) {
-
-				if (this.silver == o.silver) {
-					return o.bronze - this.bronze;
-				}
-				return o.silver - this.silver;
-			}
-
-			return o.gold - this.gold;
-		}
-	}
-
-	static Nation[] nations;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -38,41 +11,43 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		nations = new Nation[N];
+		int[][] medal = new int[3][N + 1];
 
-		PriorityQueue<Nation> pq = new PriorityQueue<>();
 		int idx, n1, n2, n3;
-
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			idx = Integer.parseInt(st.nextToken());
 			n1 = Integer.parseInt(st.nextToken());
 			n2 = Integer.parseInt(st.nextToken());
 			n3 = Integer.parseInt(st.nextToken());
-			nations[i] = new Nation(idx, n1, n2, n3);
-			pq.offer(nations[i]);
+
+			medal[0][idx] = n1;
+			medal[1][idx] = n2;
+			medal[2][idx] = n3;
 		}
-		
-		int res = 1, curr = 0;
-		for (Nation nation : pq) {
-			if (nation.num == K) {
-				curr = res - 1;
-				break;
+
+		int rank = 1;
+		for (int i = 1; i < N + 1; i++) {
+
+			if (i == K) {
+				continue;
 			}
-			res++;
-		}
-		
-		for (int i = curr - 1; i >= 0; i--) {
-			if (nations[i].gold == nations[curr].gold) {
-				if (nations[i].silver == nations[curr].silver) {
-					if (nations[i].bronze == nations[curr].bronze) {
-						res--;
-						continue;
+
+			if (medal[0][i] > medal[0][K]) {
+				rank++;
+			} else if (medal[0][i] == medal[0][K]) {
+				if (medal[1][i] > medal[1][K]) {
+					rank++;
+				} else if (medal[1][i] == medal[1][K]) {
+					if (medal[2][i] > medal[2][K]) {
+						rank++;
 					}
+
 				}
 			}
-			break;
 		}
-		System.out.println(res);
+
+		System.out.println(rank);
+
 	}
 }
