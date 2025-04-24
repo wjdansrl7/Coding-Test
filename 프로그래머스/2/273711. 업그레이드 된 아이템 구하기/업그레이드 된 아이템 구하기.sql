72be635498e12017bea2c73c51904bdb1a2576c6
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-04-25 02:36:23 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+select a.item_id, a.item_name, a.rarity
+from item_info a join item_tree b
+on a.item_id = b.item_id
+where b.parent_item_id in 
+    (select a.item_id
+    from item_info a join item_tree b
+    on a.item_id = b.item_id
+    where a.rarity = 'RARE'
+    )
+order by a.item_id desc;
