c2b0593823a4c027dccdf0495b8e7a692421effe
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-05-15 01:31:02 +0900

    [Gold IV] Title: 좋다, Time: 120 ms, Memory: 12716 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,40 +1,50 @@
+
 import java.io.*;
 import java.util.*;
 
+/**
+ *packageName    : _250514
+ * fileName       : BOJ_G4_1253_좋다
+ * author         : moongi
+ * date           : 5/14/25
+ * description    :
+ */
 public class Main {
-    static int N;
-    static int[] arr;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        int res = 0;
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        for (int i = 0; i < N; i++) {
-            int l = 0, r = N - 1;
-            while (true) {
-                if (l == i) l++;
-                else if (r == i) r--;
-                if (l >= r) break;
-                if (arr[i] < arr[l] + arr[r]) {
-                    r--;
-                } else if (arr[i] > arr[l] + arr[r]) {
-                    l++;
-                } else {
-                    if (r != i && l != i) {
-                        res++;
-                        break;
-                    }
-                }
-            }
-        }
-        System.out.println(res);
-    }
+		Arrays.sort(arr);
+
+		int res = 0;
+
+		for (int i = 0; i < N; i++) {
+			int left = 0;
+			int right = N - 1;
+			while (true) {
+
+				if (left == i) left++;
+				else if (right == i) right--;
+
+				if (left >= right) break;
+
+				int target = arr[left] + arr[right];
+				if (target > arr[i]) right--;
+				else if (target < arr[i]) left++;
+				else {
+					res++;
+					break;
+				}
+			}
+		}
+
+		System.out.println(res);
+	}
 }
