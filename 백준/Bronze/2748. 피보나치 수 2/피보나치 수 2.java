b7c9bfe647355aac9cf1e904b73175c8897efea1
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-05-18 13:45:21 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,19 +1,30 @@
-import java.util.*;
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *packageName    : _250518
+ * fileName       : BOJ_B1_2748_피보나치수2
+ * author         : moongi
+ * date           : 5/18/25
+ * description    :
+ */
 public class Main {
-    static long[] dp;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[n+1];
 
-        dp = new long[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+		dp[0] = 0;
+		dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
-        System.out.println(dp[n]);
-    }
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		
+		sb.append(dp[n]);
+		System.out.println(sb);
+	}
 }
