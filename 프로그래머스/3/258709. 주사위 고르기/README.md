4e4e8f2cb720020e7e9fc40ca36d1638e54cae5a
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-06-05 11:56:05 +0900

    [level 3] Title: 주사위 고르기, Time: 703.57 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/README.md
+++ 프로그래머스/3/258709. 주사위 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 주사위 고르기 - 258709 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709#qna) 
 
 ### 성능 요약
 
-메모리: 133 MB, 시간: 753.14 ms
+메모리: 143 MB, 시간: 703.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 05일 11:22:55
+2025년 06월 05일 11:56:02
 
 ### 문제 설명
 
--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,100 +1,129 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
-    static int N;
-    static List<Integer> choice = new ArrayList<>();
-    static int[][] dices;
-    static List<Integer> arrA;
-    static List<Integer> arrB;
-    static int[] answer;
-    static int max = Integer.MIN_VALUE;
-
-    public static void makeArr(int depth, int[][] dice, int sum, List<Integer> arr) {
-        if (depth == N / 2) {
+    
+    // 주사위 선택
+    static void pickDice(int cnt, int start, int[][] dice) {
+        
+        if(cnt == N / 2) {
+            
+            int winCount = calculateWin(dice);
+            
+            if(max < winCount) {
+                max = winCount;
+                
+                for(int i = 0; i < selected.size(); i++) {
+                    result[i] = selected.get(i) + 1;
+                }
+            }
+            
+            return;
+        }
+        
+        for(int i = start; i < N; i++) {
+            
+            selected.add(i);
+            pickDice(cnt+1, i+1, dice);
+            selected.remove(selected.size()-1);
+        }
+        
+    }
+    
+    static int calculateWin(int[][] dice) {
+        
+        int count = 0;
+        
+        makeArrAB(dice);
+        
+        // arrA, arrB에 대하여 승패를 결정한다.
+        
+        Collections.sort(arrB);
+        
+        // 승부를 결정할 때, 이분 탐색을 통하여 시간복잡도를 줄인다. O(6^5 * log 6^5)
+        
+        for(int i = 0; i < arrA.size(); i++) {
+            
+            int target = arrA.get(i);
+            
+            int st = 0;
+            int et = arrB.size() - 1;
+            
+            int idx = -1;
+            while(st <= et) {
+                
+                int mid = (st + et) >> 1;
+                
+                if(target <= arrB.get(mid)) {
+                    et = mid - 1;
+                } else {
+                    idx = Math.max(idx, mid);
+                    st = mid + 1;
+                }
+                
+            }
+            if(idx != -1) count += idx + 1;
+            
+        }
+        
+        return count;
+    }
+    
+    
+    // 선택된 주사위들의 합 분포를 저장
+    static void sumDice(int cnt, int sum, int[][] dice, List<Integer> arr) {
+        
+        if(cnt == N / 2) {
             arr.add(sum);
             return;
         }
-        for (int i = 0; i < 6; i++) {
-            int newSum = sum + dice[depth][i];
-            makeArr(depth + 1, dice, newSum, arr);
-        }
+        
+        for(int i = 0; i < 6; i++) {
+            int newSum = sum + dice[cnt][i];
+            sumDice(cnt+1, newSum, dice, arr);
+        }   
     }
-
-    public static void makeArrAB() {
+    
+    static void makeArrAB(int[][] dice) {
+        
+        // 선택된 주사위들의 합 분포를 저장한 배열
         arrA = new ArrayList<>();
         arrB = new ArrayList<>();
-
+        
         int[][] diceA = new int[N / 2][6];
         int[][] diceB = new int[N / 2][6];
+        
         int a = 0, b = 0;
-        for (int i = 0; i < N; i++) { // diceA, diceB 만듦
-            if (choice.contains(i)) { // diceA
-                diceA[a] = dices[i];
-                a++;
-            } else { // diceB
-                diceB[b] = dices[i];
-                b++;
+        for(int i = 0; i < N; i++) {
+            
+            if(selected.contains(i)) {
+                diceA[a++] = dice[i];
+            } else {
+                diceB[b++] = dice[i];
             }
+            
         }
-
-        makeArr(0, diceA, 0, arrA);
-        makeArr(0, diceB, 0, arrB);
+        
+        sumDice(0, 0, diceA, arrA);
+        sumDice(0, 0, diceB, arrB);
+        
     }
-
-    public static int calculateWinningPercent() {
-        int count = 0;
-
-        makeArrAB(); // 이제 arrA, arrB에 모든 경우의 수 저장됨..!
-
-        Collections.sort(arrB);
-        for (int i = 0; i < arrA.size(); i++) {
-            // 이분 탐색으로 arrB에 number 보다 작은 숫자가 몇개 있는지 구한다.
-            int number = arrA.get(i);
-
-            int left = 0, right = arrB.size() - 1;
-
-            int index = Integer.MIN_VALUE;
-            while (left <= right) {
-                int middle = (left + right) / 2;
-
-                if (arrB.get(middle) < number) { // 조건 만족: number 보다 작아야 함!! 같아도 안됨.
-                    left = middle + 1;
-                    index = Math.max(index, middle);
-                } else {
-                    right = middle - 1;
-                }
-            }
-            if (index != Integer.MIN_VALUE) {
-                count += index + 1;
-            }
-        }
-        return count;
-    }
-
-    public static void choiceDice(int depth, int s) {
-        if (depth == N / 2) {
-            int winning = calculateWinningPercent();
-//            System.out.println(choice.get(0) + ", " + choice.get(1) + " => " + winning);
-            if (max < winning) {
-                max = winning;
-                for (int i = 0; i < choice.size(); i++) {
-                    answer[i] = choice.get(i) + 1;
-                }
-            }
-            return;
-        }
-        for (int i = s; i < N; i++) {
-            choice.add(i);
-            choiceDice(depth + 1, i + 1);
-            choice.remove(choice.size() - 1);
-        }
-    }
+    
+    
+    
+    static int N, max = 0;
+    static int[] result;
+    static List<Integer> selected, arrA, arrB;
     public int[] solution(int[][] dice) {
         N = dice.length;
-        dices = dice;
-        answer = new int[N / 2];
-        choiceDice(0, 0);
-        return answer;
+        result = new int[N / 2];
+        
+        selected = new ArrayList<>();
+        
+        pickDice(0, 0, dice);
+        
+        
+        return result;
+        
     }
 }
