547dda9e48b5d1e659be22030fb00621ad9a485d
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-06-24 20:12:12 +0900

    [Gold IV] Title: 고층 건물, Time: 64 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -1,43 +1,59 @@
-import java.io.*;
-import java.util.*;
 
+import java.util.*;
+import java.io.*;
+
+/**
+ *packageName    : _250624
+ * fileName       : BOJ_G4_1027_고층건물
+ * author         : moongi
+ * date           : 6/24/25
+ * description    :
+ */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
 		int[] height = new int[N];
-		int max = 0;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
-		// 이전 기울기보다 작아지면
+		
+		// 더 작아져야 보임
 		for (int i = 0; i < N; i++) {
-			double curve = Double.MAX_VALUE;
-            for (int j = i-1; j >= 0; j--) {
-                if (curve > (double) (arr[i] - arr[j]) / (i - j)) {
-                    curve = (double) (arr[i] - arr[j]) / (i - j);
-                    height[i]++;
-                }
-            }
+			double dist = Double.MAX_VALUE;
 
-            curve = -Double.MAX_VALUE;
-            for (int j = i+1; j < N; j++) {
-                if (curve < (double) (arr[j] - arr[i]) / (j - i)) {
-                    curve = (double) (arr[j] - arr[i]) / (j - i);
-                    height[i]++;
-                }
-            }
+			// 타겟 건물 이전 위치
+			for (int j = i - 1; j >= 0; j--) {
 
-			if (height[i] > max) {
-				max = height[i];
+				double tmp = (double)(arr[i] - arr[j]) / (i - j);
+
+				if (dist > tmp) {
+					dist = tmp;
+					height[i]++;
+				}
+			}
+
+			dist = -Integer.MAX_VALUE;
+
+			// 타겟 건물 이후 위치
+			for (int j = i + 1; j < N; j++) {
+
+				double tmp = (double)(arr[j] - arr[i]) / (j - i);
+
+				if (dist < tmp) {
+					dist = tmp;
+					height[i]++;
+				}
 			}
 		}
 
-		System.out.println(max);
+		Arrays.sort(height);
+
+		System.out.println(height[N-1]);
 	}
 }
