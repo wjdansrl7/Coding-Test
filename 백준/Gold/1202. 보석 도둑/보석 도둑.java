90aa4dfe25891787137d65ecf93c8132b63925bf
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-06-27 14:10:31 +0900

    [Gold II] Title: 보석 도둑, Time: 1592 ms, Memory: 131076 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,61 +1,84 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 /**
- * packageName    : _241021
+ *packageName    : _250627
  * fileName       : BOJ_G2_1202_보석도둑
  * author         : moongi
- * date           : 10/21/24
+ * date           : 6/27/25
  * description    :
- * Comparable -> compareTo(Object o)를 사용하고, Collections.sort()에서 사용할 수 있다.
- * Comparator -> compare(Object o1, Object o2)를 사용하고 Collections.sort(DataStructure, new customComparator());
- *
  */
 public class Main {
-    static class Jewelry implements Comparable<Jewelry>{
-        int m, v;
+	static class Jewel implements Comparable<Jewel> {
+		int weight, price;
 
-        public Jewelry(int m, int v) {
-            this.m = m;
-            this.v = v;
-        }
-        @Override
-        public int compareTo(Jewelry o) {
-            if (this.m == o.m) return this.v - o.v;
-            return this.m - o.m;
-        }
-    }
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		public Jewel(int weight, int price) {
+			this.weight = weight;
+			this.price = price;
+		}
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+		@Override
+		public int compareTo(Jewel o) {
+			if (this.weight == o.weight) {
+				return o.price - this.price;
+			}
+			return this.weight - o.weight;
+		}
+	}
 
-        List<Jewelry> jewelries = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            jewelries.add(new Jewelry(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Collections.sort(jewelries);
+		int N = Integer.parseInt(st.nextToken()); // 보석의 개수
+		int K = Integer.parseInt(st.nextToken()); // 가방의 개수
 
-        int[] bags = new int[K];
+		Jewel[] jewels = new Jewel[N];
 
-        for (int i = 0; i < K; i++) {
-            bags[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(bags);
-        long res = 0;
-        // 보석 가격을 저장하는 pq -> 가장 작은 무게의 가방부터 순서대로 보석들의 무게와 비교하여 넣을 수 있다면, 보석을 pq에 집어넣는다.
-        // 이때, 보석의 가격의 합이 내림차순으로 저장되기 때문에, 마지막에 해당 가방에 넣을 수 있는 보석 중에 가장 비싼 보석이 첫 번째 보석이 된다.
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0, j = 0; i < K; i++) {
-            while (j < N && jewelries.get(j).m <= bags[i]) {
-                pq.offer(jewelries.get(j++).v);
-            }
-            if (!pq.isEmpty()) res += pq.poll();
-        }
-        System.out.println(res);
-    }
+		int M, V;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			M = Integer.parseInt(st.nextToken()); // 보석의 무게
+			V = Integer.parseInt(st.nextToken()); // 보석의 가격
+			jewels[i] = new Jewel(M, V);
+		}
+
+		Arrays.sort(jewels);
+
+		int C;
+		int[] bags = new int[K];
+		for (int i = 0; i < K; i++) {
+			bags[i] = Integer.parseInt(br.readLine()); // 가방의 무게
+		}
+
+		Arrays.sort(bags);
+
+		// 조건
+		// 1. 보석의 무게는 가방의 무게를 넘지 못한다.
+		// 2. 가방 하나에는 보석 하나만 담을 수 있고, 이때 보석의 최대 가격을 구하는 문제
+
+		// 가방의 무게를 정렬해서 가능한 보석을 모두 pq에 넣는다.
+		// 이후 가방들은 공간이 이전보다 다 넉넉하기 때문에, 모두 수용가능할 것이기 때문이다.
+
+		// 그래서 넣을 수 있는 보석을 다 넣고 각각의 가방에 대해 우선순위큐에 들어있는 가장 비싼
+		// 보석을 하나 꺼내서 가방에 넣는다고 가정하고, 결과값을 갱신해준다.
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		long res = 0;
+
+		for (int i = 0, j = 0; i < K; i++) {
+
+			while(j < N && jewels[j].weight <= bags[i]) {
+				pq.offer(jewels[j++].price);
+			}
+
+			if (!pq.isEmpty()) {
+				res += pq.poll();
+			}
+		}
+
+		System.out.println(res);
+
+	}
 }
