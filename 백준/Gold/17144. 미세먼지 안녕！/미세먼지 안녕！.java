27f1e024bc1aa5ae0fa916b7221a084823e603af
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-03 14:55:22 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 284 ms, Memory: 28760 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,114 +1,154 @@
-import java.io.*;
+
 import java.util.*;
+import java.io.*;
 
+/**
+ *packageName    : _250703
+ * fileName       : BOJ_G4_17144_미세먼지안녕
+ * author         : moongi
+ * date           : 7/3/25
+ * description    :
+ */
 public class Main {
-    static int R, C, T;
-    static int[][] arr;
-    static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb;
+	static int R, C, T;
+	static int[][] board;
+	static int[] conditioner;
 
-        st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        arr = new int[R][C];
-        for (int i = 0; i < R; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < C; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            spreadDust();
-            airCondition();
-        }
+		board = new int[R][C];
+		conditioner = new int[2];
 
-        int res = 0;
-        for (int i = 0; i < R; i++)
-            for (int j = 0; j < C; j++)
-                res += arr[i][j];
-        System.out.println(res+2);
-    }
-    static void spreadDust() {
-        int[][] tmp = new int[R][C];
+		int idx = 0;
+		for (int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < C; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                if (arr[i][j] > 0) {
-                    int v = arr[i][j];
-                    for (int d = 0; d < 4; d++) {
-                        int nx = i + dx[d];
-                        int ny = j + dy[d];
-                        if (nx >= 0 && nx < R && ny >= 0 && ny < C && arr[nx][ny] >= 0) {
-                            v -= arr[i][j]/5;
-                            tmp[nx][ny] += arr[i][j] / 5;
-                        }
-                    }
-                    arr[i][j] = v;
-                }
-            }
-        }
-        for (int i = 0; i < R; i++)
-            for (int j = 0; j < C; j++)
-                arr[i][j] += tmp[i][j];
-    }
-    static void airCondition() {
-        int uX = 0, uY = 0, dX = 0, dY = 0;
-        for (int i = 0; i < R; i++) {
-            if(arr[i][0] == -1) {
-                uX = i;
-                uY = 0;
-                dX = i+1;
-                dY = 0;
-                break;
-            }
-        }
-        
-        dx = new int[]{0, -1, 0, 1}; dy = new int[]{1, 0, -1, 0};
-        int tmp1 = arr[uX][C - 1];
-        int tmp2 = arr[0][C - 1];
-        int tmp3 = arr[0][0];
+				// 공기 청정기의 위치 저장
+				if (board[i][j] == -1) {
+					conditioner[idx++] = i;
+				}
+			}
+		}
 
-        for (int i = C-1; i > 1; i--)
-            arr[uX][i] = arr[uX][i - 1];
-        arr[uX][1] = 0;
+		int time = 0;
+		while(time < T) {
 
-        for (int i = 0; i < uX-1; i++)
-            arr[i][C - 1] = arr[i + 1][C - 1];
-        arr[uX - 1][C - 1] = tmp1;
+			moveDust();
+			moveAir(conditioner);
+			
+			// 1초 경과
+			time++;
+		}
 
-        for (int i = 0; i < C - 2; i++)
-            arr[0][i] = arr[0][i + 1];
-        arr[0][C - 2] = tmp2;
+		int ans = 0;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				ans += board[i][j];
 
-        for (int i = uX-1; i > 1; i--)
-            arr[i][0] = arr[i - 1][0];
-        arr[1][0] = tmp3;
+			}
+		}
 
-        dx = new int[]{0, 1, 0, -1}; dy = new int[]{1, 0, -1, 0};
+		sb.append(ans + 2);
+		System.out.println(sb);
+		
+	}
 
-        tmp1 = arr[dX][C - 1];
-        tmp2 = arr[R - 1][C - 1];
-        tmp3 = arr[R - 1][0];
-        for (int i = C-1; i > 1; i--)
-            arr[dX][i] = arr[dX][i - 1];
-        arr[dX][1] = 0;
+	static void moveDust() {
+		int[][] tmp = new int[R][C];
 
-        for (int i = R-1; i > dX+1; i--)
-            arr[i][C - 1] = arr[i - 1][C - 1];
-        arr[dX+1][C - 1] = tmp1;
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
 
-        for (int i = 0; i < C - 2; i++)
-            arr[R - 1][i] = arr[R - 1][i + 1];
-        arr[R - 1][C - 2] = tmp2;
+				int sum = 0;
+				int part = board[i][j] / 5;
 
-        for (int i = dX+1; i < R - 2; i++)
-            arr[i][0] = arr[i + 1][0];
-        arr[R - 2][0] = tmp3;
-    }
+				for (int d = 0; d < 4; d++) {
+					int nx = i + dx[d];
+					int ny = j + dy[d];
+
+					if (!isValid(nx, ny)) continue;
+
+					tmp[nx][ny] += part;
+					sum += part;
+
+				}
+
+				tmp[i][j] += board[i][j] - sum;
+
+			}
+		}
+
+		// 먼지 확산 이후
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				board[i][j] = tmp[i][j];
+			}
+		}
+
+		// 공기 청정기 위치
+		board[conditioner[0]][0] = -1;
+		board[conditioner[1]][0] = -1;
+
+	}
+
+	static boolean isValid(int x, int y) {
+		if (x >= R || x < 0  || y >= C || y < 0 || board[x][y] == -1) return false;
+
+		return true;
+	}
+
+	static void moveAir(int[] conditioner) {
+		// 모두 한 칸씩 이동하며, 공기 청정기 위치에 도착 시, 해당 먼지는 제거
+
+		// 위쪽 공기 청정기: 반시계 방향
+
+		for (int i = conditioner[0] - 1; i >= 1; i--) {
+			board[i][0] = board[i - 1][0];
+		}
+
+		for (int i = 0; i < C - 1; i++) {
+			board[0][i] = board[0][i + 1];
+		}
+
+		for (int i = 0; i < conditioner[0]; i++) {
+			board[i][C - 1] = board[i + 1][C - 1];
+		}
+
+		for (int i = C - 1; i > 1; i--) {
+			board[conditioner[0]][i] = board[conditioner[0]][i - 1];
+		}
+
+		board[conditioner[0]][1] = 0;
+
+		// 아래쪽 공기 청정기: 시계 방향
+
+		for (int i = conditioner[1] + 1; i < R - 1; i++) {
+			board[i][0] = board[i + 1][0];
+		}
+
+		for (int i = 0; i < C - 1; i++) {
+			board[R - 1][i] = board[R - 1][i + 1];
+		}
+
+		for (int i = R-1; i > conditioner[1]; i--) {
+			board[i][C - 1] = board[i - 1][C - 1];
+		}
+
+		for (int i = C - 1; i > 1; i--) {
+			board[conditioner[1]][i] = board[conditioner[1]][i - 1];
+		}
+
+		board[conditioner[1]][1] = 0;
+	}
 }
