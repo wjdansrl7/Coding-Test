a0dc2180d680d60fabd3e639085a40dd24fab39d
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-05 14:21:09 +0900

    [Gold IV] Title: 부분합, Time: 184 ms, Memory: 22368 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,70 +1,45 @@
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 /**
- *packageName    : _250627
- * fileName       : BOJ_G4_1806
+ *packageName    : _250703
+ * fileName       : BOJ_G4_1806_부분합
  * author         : moongi
- * date           : 6/27/25
+ * date           : 7/5/25
  * description    :
  */
 public class Main {
+	static int N, S;
+	static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[N];
-
-		// 길이를 이분 탐색한다.
+		arr = new int[N];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int res = 100_001;
-		int left = 0, right = N;
+		int ans = Integer.MAX_VALUE;
+		int left = 0;
+		long sum = 0;
 
-		while (left <= right) {
+		for (int right = 0; right < N; right++) {
+			sum += arr[right];
 
-			int mid = (left + right) >> 1;
-
-			int sum = 0;
-			for (int i = 0; i < mid; i++) {
-				sum += arr[i];
-			}
-
-			if (sum >= S) {
-				right = mid - 1;
-				if(res > mid) res = mid;
-				continue;
-			}
-
-			boolean flag = false;
-			for (int i = mid, j = 0; i < N; i++, j++) {
-				sum -= arr[j];
-				sum += arr[i];
-
-				if (sum >= S) {
-					right = mid - 1;
-					if(res > mid) res = mid;
-					flag = true;
-					break;
-				}
-			}
-
-			if (!flag) {
-				left = mid + 1;
+			while (sum >= S) {
+				ans = Math.min(ans, right - left + 1);
+				sum -= arr[left++];
 			}
 		}
 
-		if (res == 100_001) res = 0;
-
-		sb.append(res);
-		System.out.println(res);
+		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
+		
 	}
 }
