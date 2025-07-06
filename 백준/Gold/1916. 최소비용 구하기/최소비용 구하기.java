5e412d490c8dc0b41250dbe141e484b1dc21d250
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-06 18:49:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 452 ms, Memory: 52512 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,74 +1,92 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
+/**
+ *packageName    : _250706
+ * fileName       : BOJ_G5_1916_최소비용구하기
+ * author         : moongi
+ * date           : 7/6/25
+ * description    :
+ */
 public class Main {
-    static int N, M;
+	static class Node implements Comparable<Node> {
+		int to, weight;
 
-    static class Node implements Comparable<Node>{
-        int to, weight;
+		public Node(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
 
-        public Node(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
-    }
+		@Override
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
+		}
+	}
 
-    static List<Node>[] nodes;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 
-        nodes = new List[N + 1];
-        for (int i = 0; i < N + 1; i++) nodes[i] = new ArrayList<>();
-        int[] dist = new int[N + 1];
-        boolean[] v = new boolean[N + 1];
+		List<Node>[] graphs = new List[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			graphs[i] = new ArrayList<>();
+		}
 
-        int from, to, weight;
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            from = Integer.parseInt(st.nextToken());
-            to = Integer.parseInt(st.nextToken());
-            weight = Integer.parseInt(st.nextToken());
-            nodes[from].add(new Node(to, weight));
-        }
+		int a, b, c;
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N + 1; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
 
-        int start, end;
-        st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+			graphs[a].add(new Node(b, c));
+		}
 
-        dist[start] = 0;
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int end = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+		int[] dist = new int[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
 
-        pq.offer(new Node(start, dist[start]));
+		dist[start] = 0;
 
-        while (!pq.isEmpty()) {
-            Node p = pq.poll();
-            int cur = p.to;
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[N + 1];
+		pq.offer(new Node(start, 0));
+		
 
-            if (v[cur]) continue;
-            v[cur] = true;
-            
-            for (Node node : nodes[cur]) {
-                if (!v[node.to] && dist[node.to] > dist[cur] + node.weight) {
-                    dist[node.to] = dist[cur] + node.weight;
-                    pq.offer(new Node(node.to, dist[node.to]));
-                }
-            }
-        }
-        System.out.println(dist[end]);
-    }
+		while (!pq.isEmpty()) {
+
+			Node curr = pq.poll();
+			
+			if (!visited[curr.to]) {
+				
+				visited[curr.to] = true;
+				
+				for (Node next : graphs[curr.to]) {
+	
+					if (!visited[next.to] && dist[next.to] > dist[curr.to] + next.weight) {
+						dist[next.to] = dist[curr.to] + next.weight;
+
+						pq.offer(new Node(next.to, dist[curr.to] + next.weight));
+					}
+				}
+				
+			}
+
+		}
+
+		sb.append(dist[end]);
+		System.out.println(sb);
+
+	}
 }
