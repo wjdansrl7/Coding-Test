c001ad25fdaeeaec04bd5da062836e93341e52d8
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-09 15:47:19 +0900

    [Silver V] Title: 다리 놓기, Time: 68 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,26 +1,41 @@
-import java.util.*;
+
 import java.io.*;
-
+import java.util.*;
+/**
+ *packageName    : _250709
+ * fileName       : BOJ_S5_1010_다리놓기
+ * author         : moongi
+ * date           : 7/9/25
+ * description    :
+ */
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
-        int N, M;
-        for (int tc = 0; tc < T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
+		while (T-- > 0) {
 
-            if (M / 2 < N) N = M - N;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-            long res = 1;
+			if (M / 2 < N) N = M - N;
 
-            for (int i = M-N+1; i <= M; i++) res *= i;
-            for (int i = 1; i <= N; i++) res /= i;
-            System.out.println(res);
-        }
-    }
+			long sum = 1;
+
+			for (int i = M - N + 1; i <= M; i++) {
+				sum *= i;
+			}
+
+			for (int i = 1; i <= N; i++) {
+				sum /= i;
+			}
+
+			sb.append(sum).append('\n');
+		}
+
+		System.out.println(sb);
+	}
 }
