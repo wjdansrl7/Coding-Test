41350098686290b34bcb1728917e3a66064c1b5d
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-11 14:17:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,30 +1,27 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
+import java.util.*;
+import java.io.*;
 /**
- *packageName    : _250518
+ *packageName    : _250711
  * fileName       : BOJ_B1_2748_피보나치수2
  * author         : moongi
- * date           : 5/18/25
+ * date           : 7/11/25
  * description    :
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		long[] dp = new long[n+1];
+		int N = Integer.parseInt(br.readLine());
 
+		long[] dp = new long[N + 1];
 		dp[0] = 0;
 		dp[1] = 1;
 
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i < N + 1; i++) {
 			dp[i] = dp[i - 1] + dp[i - 2];
 		}
-		
-		sb.append(dp[n]);
-		System.out.println(sb);
+
+		System.out.println(dp[N]);
 	}
 }
