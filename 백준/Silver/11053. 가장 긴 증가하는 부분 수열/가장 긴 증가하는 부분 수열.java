93bc74bdd337d23f33042cbf339f54f38ab6bcfb
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-30 16:37:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 68 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,61 @@
-
 import java.io.*;
 import java.util.*;
+
 /**
- *packageName    : _250709
+ *packageName    : _250730
  * fileName       : BOJ_S2_11053_가장긴증가하는부분수열
  * author         : moongi
- * date           : 7/9/25
+ * date           : 7/30/25
  * description    :
  */
 public class Main {
+	static int[] dp;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		int[] dp = new int[N];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
+		dp = new int[N];
+		int LIS = 0;
+
 		for (int i = 0; i < N; i++) {
-			int max = 0;
-			for (int j = 0; j < i; j++) {
-				if (arr[i] > arr[j]) {
-					if (max < dp[j]) max = dp[j];
-				}
+			int idx = lowerBound(arr[i], 0, LIS, LIS + 1); // num, start, end, size
+
+			if (idx == -1) {
+				dp[LIS++] = arr[i];
+			} else {
+				dp[idx] = arr[i];
 			}
-			dp[i] = max + 1;
 		}
 
-		Arrays.sort(dp);
-		System.out.println(dp[N-1]);
+		System.out.println(LIS);
+		
+	}
+
+	static int lowerBound(int num, int start, int end, int size) {
+		int res = 0;
+
+		while (start <= end) {
+
+			int mid = (start + end) >> 1;
+
+			if (num <= dp[mid]) {
+				res = mid;
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+
+		if (start == size) return -1;
+		else return res;
+
 	}
 }
