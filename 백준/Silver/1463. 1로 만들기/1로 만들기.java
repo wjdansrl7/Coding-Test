f9778a488543ffcbcb53796f4f9010351fa49e9c
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-07-30 16:05:48 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15648 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,58 +1,42 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 /**
- *packageName    : _250709
+ *packageName    : _250730
  * fileName       : BOJ_S3_1463_1로만들기
  * author         : moongi
- * date           : 7/9/25
+ * date           : 7/30/25
  * description    :
  */
 public class Main {
-	static final int INF = 1_000_000;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int X = Integer.parseInt(br.readLine());
-		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return Integer.compare(o1[0], o2[0]);
-			}
-		});
+		int N = Integer.parseInt(br.readLine());
 
-		pq.offer(new int[] {X, 0});
-		int ans = INF + 1;
-		int[] count = new int[INF + 1];
-		Arrays.fill(count, INF + 1);
+		int[] dp = new int[N + 1];
 
-		while (!pq.isEmpty()) {
+		dp[1] = 0;
 
-			int[] p = pq.poll();
-
-			if (p[0] == 1) {
-				if (ans > p[1])
-					ans = p[1];
-				continue;
-			}
-
-			if (p[0] % 3 == 0 && count[p[0] / 3] > p[1] + 1) {
-				count[p[0] / 3] = p[1] + 1;
-				pq.offer(new int[] {p[0] / 3, p[1] + 1});
-			}
-
-			if (p[0] % 2 == 0 && count[p[0] / 2] > p[1] + 1) {
-				count[p[0] / 2] = p[1] + 1;
-				pq.offer(new int[] {p[0] / 2, p[1] + 1});
-			}
-
-			if (count[p[0] - 1] > p[1] + 1) {
-				count[p[0] - 1] = p[1] + 1;
-				pq.offer(new int[] {p[0] - 1, p[1] + 1});
-			}
-
+		if (N <= 1) {
+			System.out.println(dp[N]);
+			return;
 		}
 
-		System.out.println(ans);
+		for (int i = 2; i < N + 1; i++) {
+
+			int tmp1 = Integer.MAX_VALUE, tmp2 =Integer.MAX_VALUE;
+			if (i % 2 == 0) {
+				tmp1 = dp[i / 2];
+			}
+
+			if (i % 3 == 0) {
+				tmp2 = dp[i / 3];
+			}
+
+			dp[i] = Math.min(Math.min(tmp1, tmp2), dp[i - 1]) + 1;
+		}
+
+		System.out.println(dp[N]);
 	}
 }
