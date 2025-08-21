1f7161aa17863d3c1dbbdb1fa3d0580df7e93335
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-08-21 15:48:49 +0900

    [Gold III] Title: 이진 트리, Time: 448 ms, Memory: 170560 KB -BaekjoonHub

--- 백준/Gold/13325. 이진 트리/이진 트리.java
+++ 백준/Gold/13325. 이진 트리/이진 트리.java
@@ -1,53 +1,49 @@
 import java.util.*;
 import java.io.*;
 /**
- *packageName    : _250813
+ *packageName    : _250821
  * fileName       : BOJ_G3_13325_이진트리
  * author         : moongi
- * date           : 8/13/25
+ * date           : 8/21/25
  * description    :
- * // 2 2 3 3 3 3
  */
 public class Main {
 	static int size, ans;
-	static int[] tree;
+	static int[] nodes;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int K = Integer.parseInt(br.readLine());
-		ans = 0;
-
-		// 트리 노드의 개수
-		size = (int) Math.pow(2, K + 1) - 1; // 7
-
-		tree = new int[size + 1];
+		size = (int)Math.pow(2, K + 1) - 1;
+		nodes = new int[size + 1]; // 트리의 개수
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		for (int i = 2; i <= size; i++) {
-			tree[i] = Integer.parseInt(st.nextToken());
+			nodes[i] = Integer.parseInt(st.nextToken());
 		}
 
+		ans = 0;
+
 		dfs(1);
+
 		System.out.println(ans);
 
 	}
 
-	static int dfs(int node) {
+	static int dfs(int start) {
 
-		if (node * 2 >= size) {
-			// 리프노드의 값을 더해주고 반환한다.
-			ans += tree[node];
-			return tree[node];
+		// 리프노드의 값을 더해주고 반환한다.
+		if (start * 2 >= size) {
+			ans += nodes[start];
+			return nodes[start];
 		}
 
-		int left = dfs(node * 2);
-		int right = dfs(node * 2 + 1);
+		int left = dfs(start * 2);
+		int right = dfs(start * 2 + 1);
 
-		ans += tree[node] + Math.abs(left - right);
-		return tree[node] + Math.max(left, right);
-
+		// 현재 노드의 값과 자식 노드의 차이를 더해준다.
+		// => 왼쪽 자식, 오른쪽 자식 중 작은 값을 증가시켜 값은 값으로 만든다는 것.
+		ans += nodes[start] + Math.abs(left - right);
+		return nodes[start] + Math.max(left, right);
 	}
-
-
 }
