1c94ed6aba15c6c39c6c6b9e67265b1cf567a516
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-09-10 09:31:06 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
+}
