8e156bbefe19e3f33afddae580f5bd6cfe90d097
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-09-14 10:30:28 +0900

    [level 2] Title: 구명보트, Time: 13.16 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,23 @@
 import java.util.*;
 import java.io.*;
 
+// 한번에 최대 2명
+// 50 50 70 80
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        int N = people.length;
         
         Arrays.sort(people);
         
         int j = 0;
-        for(int i = people.length - 1; i >= j; i--) {
-            if(people[i] + people[j] <= limit) {
+        for(int i = N - 1; i >= j; i--) {
+            if (people[i] + people[j] <= limit) {
                 j++;
             }
             answer++;
         }
+        
         return answer;
     }
 }
