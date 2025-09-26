fb1117682a34e9a1f79e82e07cf1a12c60492e6f
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2025-09-26 23:57:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,72 +1,59 @@
 
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 /**
- *packageName    : _250729
+ *packageName    : _250926
  * fileName       : BOJ_S1_1697_숨바꼭질
  * author         : moongi
- * date           : 7/29/25
+ * date           : 9/26/25
  * description    :
  */
 public class Main {
-
-	static class Point implements Comparable<Point> {
-
-		int x, cnt;
-
-		public Point(int x, int cnt) {
-			this.x = x;
-			this.cnt = cnt;
-		}
-
-		@Override
-		public int compareTo(Point o) {
-
-			return this.cnt - o.cnt;
-		}
-
-	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 해당 위치로부터 X - 1 or X + 1 or 2 * X
-
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int ans = 0;
 
-		PriorityQueue<Point> pq = new PriorityQueue<>();
-		boolean[] visited = new boolean[100_001];
-		pq.offer(new Point(N, 0));
-		visited[N] = true;
-
-		while (!pq.isEmpty()) {
-
-			Point p = pq.poll();
-
-			if (p.x == K) {
-				ans = p.cnt;
-				break;
-			}
-
-			if (p.x-1 >= 0 && !visited[p.x - 1]) {
-				visited[p.x - 1] = true;
-				pq.offer(new Point(p.x - 1, p.cnt + 1));
-			}
-
-			if (p.x + 1 <= 100_000 && !visited[p.x + 1]) {
-				visited[p.x + 1] = true;
-				pq.offer(new Point(p.x + 1, p.cnt + 1));
-			}
-
-			if (p.x * 2 <= 100_000 && !visited[p.x * 2]) {
-				visited[p.x*2] = true;
-				pq.offer(new Point(p.x * 2, p.cnt + 1));
-			}
+		if (N >= K) {
+			System.out.println(N - K);
+			return;
 		}
 
-		System.out.println(ans);
+		long res = Long.MAX_VALUE;
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		int[] visited = new int[100_001];
+		Arrays.fill(visited, Integer.MAX_VALUE);
+
+		q.offer(new int[] {N, 0});
+		visited[N] = 0;
+
+		while (!q.isEmpty()) {
+			int[] curr = q.poll();
+
+			if (curr[0] == K) {
+				res = res > curr[1] ? curr[1] : res;
+				continue;
+			}
+
+			if (curr[0] + 1 <= 100_000 && visited[curr[0] + 1] > curr[1] + 1) {
+				visited[curr[0] + 1] = curr[1] + 1;
+				q.offer(new int[] {curr[0] + 1, curr[1] + 1});
+			}
+
+			if (curr[0] - 1 >= 0 && visited[curr[0] - 1] > curr[1] + 1) {
+				visited[curr[0] - 1] = curr[1] + 1;
+				q.offer(new int[] {curr[0] - 1, curr[1] + 1});
+			}
+
+			if (curr[0] * 2 <= 100_000 && visited[curr[0] * 2] > curr[1] + 1) {
+				visited[curr[0] * 2] = curr[1] + 1;
+				q.offer(new int[] {curr[0] * 2, curr[1] + 1});
+			}
+
+		}
+
+		System.out.println(res);
 	}
 }
