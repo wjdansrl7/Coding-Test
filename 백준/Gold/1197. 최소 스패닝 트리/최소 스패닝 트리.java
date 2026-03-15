3a502074e57c07936a76e163ed285bc34080fbfa
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2026-03-15 18:56:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 488 ms, Memory: 60412 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,9 +1,19 @@
-
 import java.util.*;
 import java.io.*;
 
+/**
+ *packageName    : _260315
+ * fileName       : BOJ_G4_1197_최소스패닝트리
+ * author         : moongi
+ * date           : 3/15/26
+ * description    :
+ *
+ * 최소 스패닝 트리(MST, Minimum Spanning Tree)
+ * 모든 정점을 잇지만 사이클이 없는 부분 그래프
+ *
+ * V개의 모든 정점을 연결하는 간선의 수는 (V - 1)개
+ */
 public class Main {
-	static int V, E;
 	static class Node implements Comparable<Node> {
 		int to;
 		int weight;
@@ -18,52 +28,57 @@ public class Main {
 			return this.weight - o.weight;
 		}
 	}
-
 	static List<Node>[] nodes;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
 
 		nodes = new List[V + 1];
-
 		for (int i = 0; i < V + 1; i++) {
-			nodes[i] = new ArrayList<Node>();
+			nodes[i] = new ArrayList<>();
 		}
-		int a, b, w;
+
+		int A, B, C;
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			w = Integer.parseInt(st.nextToken());
 
-			nodes[a].add(new Node(b, w));
-			nodes[b].add(new Node(a, w));
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+
+			nodes[A].add(new Node(B, C));
+			nodes[B].add(new Node(A, C));
 		}
 
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		boolean[] visited = new boolean[V + 1];
-		long res = 0;
+		int res = 0;
 
+		// 임의의 시작점
 		pq.offer(new Node(1, 0));
 
 		while (!pq.isEmpty()) {
-			Node curr = pq.poll();
 
-			if (visited[curr.to]) continue;
-			visited[curr.to] = true;
+			Node cur = pq.poll();
 
-			res += curr.weight;
+			if (visited[cur.to]) continue;
 
-			for (Node node : nodes[curr.to]) {
-				if (!visited[node.to]) {
-					pq.offer(new Node(node.to, node.weight));
+			visited[cur.to] = true;
+			res += cur.weight;
+
+			for (Node next : nodes[cur.to]) {
+
+				if (!visited[next.to]) {
+					pq.offer(new Node(next.to, next.weight));
 				}
 			}
 		}
-
-		System.out.println(res);
+        
+        sb.append(res);
+		System.out.println(sb);
 	}
 }
