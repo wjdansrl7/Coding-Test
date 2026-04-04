9ad1a4c449294f2818dd6d781d240a0c7d036d6e
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2026-04-04 18:14:04 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 37112 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,42 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 /**
- *packageName    : _250905
+ *packageName    : _260404
  * fileName       : BOJ_S3_15649_N과M1
  * author         : moongi
- * date           : 9/5/25
+ * date           : 4/4/26
  * description    :
  */
 public class Main {
-	static int N, M;
-	static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		boolean[] visited = new boolean[N + 1];
-		List<Integer> li = new ArrayList<>();
-		comb(0, visited, li);
+		ArrayList<Integer> list = new ArrayList<>();
+		comb(N, M, 0, visited, list);
 
 		System.out.println(sb);
 	}
 
-	static void comb(int cnt, boolean[] visited, List<Integer> li) {
-
+	static void comb(int N, int M, int cnt, boolean[] visited, List<Integer> list) {
 		if (cnt == M) {
-			for (Integer v : li) sb.append(v).append(" ");
+			for (Integer nxt : list) sb.append(nxt).append(' ');
 			sb.append('\n');
 			return;
 		}
 
 		for (int i = 1; i < N + 1; i++) {
 			if (visited[i]) continue;
-
 			visited[i] = true;
-			li.add(i);
-			comb(cnt + 1, visited, li);
-			li.remove(li.size() - 1);
+			list.add(i);
+			comb(N, M, cnt + 1, visited, list);
+			list.remove(list.size() - 1);
 			visited[i] = false;
 		}
 	}
