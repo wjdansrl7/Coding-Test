db7c499942c3bad378520bd6a857db57943c96d2
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2026-04-05 08:01:19 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15664 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,11 +1,11 @@
+
 import java.util.*;
 import java.io.*;
-
 /**
- *packageName    : _250730
+ *packageName    : _260405
  * fileName       : BOJ_S3_1463_1로만들기
  * author         : moongi
- * date           : 7/30/25
+ * date           : 4/5/26
  * description    :
  */
 public class Main {
@@ -14,27 +14,21 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[] dp = new int[N + 1];
-
-		dp[1] = 0;
-
-		if (N <= 1) {
-			System.out.println(dp[N]);
+		if (N < 3) {
+			System.out.println(N - 1);
 			return;
 		}
 
-		for (int i = 2; i < N + 1; i++) {
+		int[] dp = new int[N + 1];
 
-			int tmp1 = Integer.MAX_VALUE, tmp2 =Integer.MAX_VALUE;
-			if (i % 2 == 0) {
-				tmp1 = dp[i / 2];
-			}
+		dp[1] = 0;
+		dp[2] = 1;
 
-			if (i % 3 == 0) {
-				tmp2 = dp[i / 3];
-			}
+		for (int i = 3; i < N + 1; i++) {
+			int v1 = i % 3 == 0 ? dp[i / 3] : Integer.MAX_VALUE;
+			int v2 = i % 2 == 0 ? dp[i / 2] : Integer.MAX_VALUE;
 
-			dp[i] = Math.min(Math.min(tmp1, tmp2), dp[i - 1]) + 1;
+			dp[i] = Math.min(Math.min(v1, v2), dp[i - 1]) + 1;
 		}
 
 		System.out.println(dp[N]);
