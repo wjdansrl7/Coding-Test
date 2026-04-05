d8fd0a6eb5cc0c206c5ea5d20c5ff16730e6e737
Moon Gi, Jeong <48114924+wjdansrl7@users.noreply.github.com>
2026-04-05 10:48:43 +0900

    [Gold IV] Title: 부분합, Time: 188 ms, Memory: 21920 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,45 +1,41 @@
 
 import java.io.*;
 import java.util.*;
-
 /**
- *packageName    : _250703
+ *packageName    : _260405
  * fileName       : BOJ_G4_1806_부분합
  * author         : moongi
- * date           : 7/5/25
+ * date           : 4/5/26
  * description    :
  */
 public class Main {
-	static int N, S;
-	static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
 
-		arr = new int[N];
-
+		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
+		int start = 0;
 		int ans = Integer.MAX_VALUE;
-		int left = 0;
-		long sum = 0;
+		long partial = 0;
 
-		for (int right = 0; right < N; right++) {
-			sum += arr[right];
+		for (int end = 0; end < N; end++) {
 
-			while (sum >= S) {
-				ans = Math.min(ans, right - left + 1);
-				sum -= arr[left++];
+			partial += arr[end];
+
+			while (partial >= S) {
+				ans = Math.min(ans, end - start + 1);
+				partial -= arr[start++];
 			}
 		}
 
 		System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
-		
 	}
 }
